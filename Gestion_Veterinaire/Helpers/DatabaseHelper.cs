807041938327eb4334997dc694d4b429227b7925
HabibRazek <habibrazeg32@gmail.com>
2024-11-19 09:28:24 +0100

    Add project files.

---
+++ Gestion_Veterinaire/Helpers/DatabaseHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+namespace Gestion_Veterinaire.Helpers
+{
+    public static class DatabaseHelper
+    {
+        private static readonly string ConnectionString;
+
+        static DatabaseHelper()
+        {
+            ConnectionString = ConfigurationManager.ConnectionStrings["DefaultConnection"]?.ConnectionString;
+
+            if (string.IsNullOrEmpty(ConnectionString))
+            {
+                throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+            }
+        }
+
+       
+        public static MySqlConnection GetConnection()
+        {
+            return new MySqlConnection(ConnectionString);
+        }
+    }
+}
